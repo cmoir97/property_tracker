c89472d1b9b721bf7610b6cf19be721b4c2c43f6
Cameron Moir <cameronjmoir@gmail.com>
2019-09-24 14:25:23 +0100

    added view all properties function

--- models/property.rb
+++ models/property.rb
@@ -3,37 +3,46 @@ require('pg')
 
 class Property
 
-attr_accessor :address, :value, :number_of_bedrooms, :year_built
-attr_reader :id
+  attr_accessor :address, :value, :number_of_bedrooms, :year_built
+  attr_reader :id
 
 
-def initialize(options)
-  @id = options['id'].to_i() if options['id']
-  @address = options['address']
-  @value = options['value'].to_i()
-  @number_of_bedrooms = options['number_of_bedrooms'].to_i()
-  @year_built = options['year_built'].to_i()
-end
+  def initialize(options)
+    @id = options['id'].to_i() if options['id']
+    @address = options['address']
+    @value = options['value'].to_i()
+    @number_of_bedrooms = options['number_of_bedrooms'].to_i()
+    @year_built = options['year_built'].to_i()
+  end
 
-def save()
-  db = PG.connect({dbname: 'property_tracker', host: 'localhost'})
-  sql =
-  "INSERT INTO propertys
-  (
-    address,
-    value,
-    number_of_bedrooms,
-    year_built
-    ) VALUES ($1, $2, $3, $4) RETURNING *"
-    values = [@address, @value, @number_of_bedrooms, @year_built]
-    db.prepare("save", sql)
-    @id = db.exec_prepared("save", values)[0]["id"].to_i
-    db.close()
+  def save()
+    db = PG.connect({dbname: 'property_tracker', host: 'localhost'})
+    sql =
+    "INSERT INTO propertys
+    (
+      address,
+      value,
+      number_of_bedrooms,
+      year_built
+      ) VALUES ($1, $2, $3, $4) RETURNING *"
+      values = [@address, @value, @number_of_bedrooms, @year_built]
+      db.prepare("save", sql)
+      @id = db.exec_prepared("save", values)[0]["id"].to_i
+      db.close()
 
-end
+    end
+
+    def Property.all()
+      db = PG.connect({dbname: 'property_tracker', host: 'localhost'})
+      sql = "SELECT * FROM propertys"
+      db.prepare("all", sql)
+      propertys = db.exec_prepared("all")
+      db.close()
+      return propertys.map { |property| Property.new(property)  }
+    end
 
 
 
 
 
-end
+  end

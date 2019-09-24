6eba22239b88fd76f57005ab0c487f3c5fcc1d66
Cameron Moir <cameronjmoir@gmail.com>
2019-09-24 13:58:36 +0100

    created property class

--- models/property.rb
+++ models/property.rb
@@ -3,6 +3,17 @@ require('pg')
 
 class Property
 
+attr_accessor :address, :value, :number_of_bedrooms, :year_built
+attr_reader :id
+
+
+def initialize(options)
+  @id = options['id'].to_i() if options['id']
+  @address = options['address']
+  @value = options['value']
+  @number_of_bedrooms = options['number_of_bedrooms']
+  @year_built = options['year_built']
+end
 
 
 
